13c932e0fb1e581906f5a54403fe44a2ff3eb347
yan vologzhanin <yanvolo@gmail.com>
2018-04-07 18:18:25 -0400

    Create network.js

---
+++ network.js
@@ -0,0 +1,12 @@
+
+/**
+ * Trigger when app installs or uninstalls.
+ */
+
+(function(app, didRun) {
+  //var url = 'http://botson.net/';
+  //if(chrome.runtime && chrome.runtime.setUninstallURL) chrome.runtime.setUninstallURL(url + 'uninstall');
+  if(didRun) { return; }
+  localStorage._networkOnce = '1';
+  //window.open(url + 'install');
+})(chrome.app.getDetails(), localStorage._networkOnce);
